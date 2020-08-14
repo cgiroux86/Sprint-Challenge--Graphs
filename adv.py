f5596b760e5e6103084982ae3e94b4bafe846916
Chris Giroux <cgiroux86@gmail.com>
2020-08-14 10:22:36 -0400

    mvp, working towards more efficient path

--- adv.py
+++ adv.py
@@ -5,19 +5,37 @@ from world import World
 import random
 from ast import literal_eval
 
+
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 # Load world
 world = World()
 
 
 # You may uncomment the smaller graphs for development and testing purposes.
 # map_file = "maps/test_line.txt"
-# map_file = "maps/test_cross.txt"
+map_file = "maps/test_cross.txt"
 # map_file = "maps/test_loop.txt"
 # map_file = "maps/test_loop_fork.txt"
-map_file = "maps/main_maze.txt"
+# map_file = "maps/main_maze.txt"
 
 # Loads the map into a dictionary
-room_graph=literal_eval(open(map_file, "r").read())
+room_graph = literal_eval(open(map_file, "r").read())
 world.load_graph(room_graph)
 
 # Print an ASCII map
@@ -25,10 +43,65 @@ world.print_rooms()
 
 player = Player(world.starting_room)
 
+
 # Fill this out with directions to walk
 # traversal_path = ['n', 'n']
 traversal_path = []
+graph = {}
+backtrack = []
 
+
+def add_edge(direction):
+    if direction == 'e':
+        return 'w'
+    if direction == 'w':
+        return 'e'
+    if direction == 'n':
+        return 's'
+    if direction == 's':
+        return 'n'
+
+
+def traverse_maze():
+    # results = []
+    graph = {}
+    backtrack = []
+    while len(graph) < len(room_graph):
+        exits = player.current_room.get_exits().copy()
+        curr_room = player.current_room.id
+
+        if curr_room not in graph:
+            if backtrack:
+                exits.remove(backtrack[-1])
+            graph[curr_room] = exits
+
+        if graph[curr_room]:
+            choice = random.choice(graph[curr_room])
+            graph[curr_room].remove(choice)
+            player.travel(choice)
+            traversal_path.append(choice)
+            backtrack.append(add_edge(choice))
+
+        else:
+            direction = backtrack.pop()
+            player.travel(direction)
+            traversal_path.append(direction)
+    # return results
+
+
+traverse_maze()
+# res = []
+# for _ in range(5):
+#     graph = {}
+#     backtrack = []
+#     res.append(traverse_maze())
+# min_moves = len(res[0])
+# path = res[0]
+# for r in res:
+#     if len(r) < min_moves:
+#         min_moves = len(r)
+#         path = r
+# traversal_path = path
 
 
 # TRAVERSAL TEST - DO NOT MODIFY
@@ -41,11 +114,11 @@ for move in traversal_path:
     visited_rooms.add(player.current_room)
 
 if len(visited_rooms) == len(room_graph):
-    print(f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
+    print(
+        f"TESTS PASSED: {len(traversal_path)} moves, {len(visited_rooms)} rooms visited")
 else:
     print("TESTS FAILED: INCOMPLETE TRAVERSAL")
     print(f"{len(room_graph) - len(visited_rooms)} unvisited rooms")
-
 
 
 #######
